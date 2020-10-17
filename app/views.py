aa0cb0bb3d2f27ad16d16e2d403dd5709dfeed0a
martinsmandina <martinsmandina@gmail.com>
2020-10-17 18:21:10 +0300

    add new route and news.html

--- app/views.py
+++ app/views.py
@@ -5,3 +5,8 @@ from app import app
 def index():
     
     return render_template('index.html')
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    return render_template('news.html',id = news_id)
