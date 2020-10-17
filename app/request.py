fdc2de015029e12d4dd862e030dd07552719b5b1
martinsmandina <martinsmandina@gmail.com>
2020-10-17 23:19:37 +0300

    change title

--- app/request.py
+++ app/request.py
@@ -1,3 +1,25 @@
 from app import app
+import urllib.request,json
+from .models import movie
+
+News = news.News
 
 api_key = app.config['NEWS_API_KEY']
+
+base_url = app.config[' NEWS_API_BASE_URL']
+
+def get_news(top_headlines):
+    get_news_url = base_url.format(top_headlines,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+        
+        news_results = None
+
+        if get_news_response['results']
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+
+    return news_results
+
--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from app import app
 @app.route('/')
 def index():
     
-    title = 'News - Local Compiled News From Kenya And Around The World'
+    title = 'News - Breaking News From Around The World'
     return render_template('index.html',title = title)
 
 @app.route('/news/<int:news_id>')
