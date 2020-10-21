e198a67a4581a76df7345c49ba992bb43736f7fc
martinsmandina <martinsmandina@gmail.com>
2020-10-21 22:33:16 +0300

    add class Article to articles.py

--- app/articles_test.py
+++ app/articles_test.py
@@ -6,7 +6,7 @@ Article = articles.Article
 class NewsTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_source = Article('self','associated_press','Associated Press','Cant crush this: Beetle armor gives clues to tougher planes','http://apnnews.com"','https://storage.googleapis.com/afs-prod/media/7de42fed748f448883a9ae11391ef2c0/2532.jpeg,'2020-10-21T18:01:29Z')
+        self.new_source = Article('self','associated_press','Associated Press','Cant crush this: Beetle armor gives clues to tougher planes','http://apnnews.com','https://storage.googleapis.com/afs-prod/media/7de42fed748f448883a9ae11391ef2c0/2532.jpeg,'2020-10-21T18:01:29Z')
         '''
         This method that will run before every Test
         '''
@@ -18,13 +18,13 @@ class NewsTest(unittest.TestCase):
         '''
         Test to confirm that objects are instaitated correctly
         '''
-        self.assertEquals(self.new_origin.id,'bloomberg')
-        self.assertEquals(self.new_origin.author,'Bloomberg')
-        self.assertEquals(self.new_origin.title,'Bloomberg delivers business and markets news')
-        self.assertEquals(self.new_origin.description,"http://www.bloomberg.com")
-        self.assertEquals(self.new_origin.url,'business')
-        self.assertEquals(self.new_origin.image,'en')
-        self.assertEquals(self.new_origin.date,'us')
+        self.assertEquals(self.new_origin.id,'associated_press')
+        self.assertEquals(self.new_origin.author,'Associated Press')
+        self.assertEquals(self.new_origin.title,'Cant crush this: Beetle armor gives clues to tougher planes')
+        self.assertEquals(self.new_origin.description,"http://apnnews.com")
+        self.assertEquals(self.new_origin.url,'http://apnnews.com')
+        self.assertEquals(self.new_origin.urlToImage,' "https://storage.googleapis.com/afs-prod/media/7de42fed748f448883a9ae11391ef2c0/2532.jpeg')
+        self.assertEquals(self.new_origin.publishedAt,'2020-10-21T18:01:29Z')
         
     
 if __name__ == '__main__':
---
+++ app/models/articles.py
@@ -0,0 +1,10 @@
+class Article:
+
+    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
